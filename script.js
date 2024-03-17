14528b274534ccf429044c36f11391a0dabcbcc7
can caliskan <podcastwerv2017@gmail.com>
2024-03-18 01:20:44 +0200

    setting up api sending post request to api get response

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const chatbox = document.querySelector(".chatbox");
 
 
 let userMessage;
+const API_KEY = "xxxx";
 
 const createChatLii = (message, className) => {
     const chatLi = document.createElement("li");
@@ -14,12 +15,37 @@ const createChatLii = (message, className) => {
 
 }
 
+const generateResponse = () => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user",content: userMessage}] 
+        })
+    }
+    fetch(API_URL,requestOptions).then(res => res.json()).then(data => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    })
+}
+
 const handleChat = () => {
     userMessage = chatInput.value.trim();
     if(!userMessage) return;
 
     chatbox.appendChild(createChatLii(userMessage, "outgoing"));
 
+    setTimeout(() =>{
+        chatbox.appendChild(createChatLii("Thinking...", "incoming"));
+        generateResponse();
+     }, 700);
 }
 
 
